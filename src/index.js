2614817b2285bcc342bdd1d7adbc10de72822274
borniequepacheco <bornieque@gmail.com>
2024-02-12 16:08:55 -0300

    desafio de projeto final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("oi");
+let nome_heroi = "Bornieque";
+let xp = 1000;
+
+
+
+
+if (xp < 1000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Ferro");
+}
+else if (xp > 1000 && xp <= 2000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Bronze");
+}
+else if (xp > 2000 && xp <= 5000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Prata");
+}
+else if (xp > 5000 && xp <= 7000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Ouro");
+}
+else if (xp > 7000 && xp <= 8000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Platina");
+}
+else if (xp > 8000 && xp <= 9000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Ascendente");
+}
+else if (xp > 9000 && xp <= 10000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Imortal");
+}
+else if (xp > 10000) {
+    console.log("O Herói de nome " + nome_heroi + " está no nível Radiante");
+}
